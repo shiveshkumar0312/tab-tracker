1f7c84fbda0ffa03199d3fef7b9986946f0573a1
Shivesh <shivesh.kumar0312@gmail.com>
2019-12-22 01:50:43 +0100

    adding register page

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,3 @@
-console.log('hello start')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -9,9 +8,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-      message: 'hello world!'  
+      message: 'Hello ' + req.body.email+ ' is a registered user!'
     })
 })
 app.listen(process.env.PORT || 8081)
